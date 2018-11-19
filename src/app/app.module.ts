cbb1e42b3f4914e3ff29236e2404a0e3c7408205
Gabriel <gabriellsl2009@hotmail.com>
2018-11-19 10:15:15 -0200

    Criação do componente header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; // PRIME NG
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
