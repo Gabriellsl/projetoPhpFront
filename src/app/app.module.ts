9c14c03cbad4eaf5e62005e8a9553dad7770c395
Gabriellsl <gabriellsl2009@hotmail.com>
2018-11-20 11:41:33 -0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { FooterComponent } from './footer/footer.component'; // PRIME NG
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component'; // PRIME NG
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
