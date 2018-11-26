e78168aa511bf437930f9fccbb9dde2d6fa0c5a7
Willian de Oliveira Silva <37350277+Willsombra@users.noreply.github.com>
2018-11-26 20:42:36 -0200

    Classe pessoa e gestor; Service pessoa
    Criados classe pessoa e gestor, e criado service pessoa (incompleto)

---
+++ src/app/model/pessoa.ts
@@ -0,0 +1,12 @@
+export class Pessoa{
+
+    id_pessoa: String;
+    nome: String;
+    email: String;
+    cpf: String;
+    rg: String;
+    login: String;
+    senha: String;
+    tipo: String;
+    
+}
