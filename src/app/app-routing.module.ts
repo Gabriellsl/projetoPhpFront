6284fe900c9ca00cc7231075a0e6c86fec5c36c7
Willian de Oliveira Silva <37350277+Willsombra@users.noreply.github.com>
2018-11-28 16:35:21 -0200

    Rota para Grafico
    Rota para grafico e alterado os minutos do grafico de 5 para 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PessoaformComponent } from './pessoaform/pessoaform.component';
 import { LoginComponent} from './login/login.component';
+import { GraficoComponent} from './grafico/grafico.component';
 
 const routes: Routes= [
   { path: 'login', component: LoginComponent},
   { path: 'formulario', component: PessoaformComponent},
-  
+  { path: 'grafico', component: GraficoComponent},
   { path: '', redirectTo: '/', pathMatch:'full'},
 ];
 
--- src/app/grafico/grafico.component.ts
+++ src/app/grafico/grafico.component.ts
@@ -82,12 +82,12 @@ export class GraficoComponent implements OnInit {
    findData(){
      this.graficoService.findData().subscribe(
       (x) => {
-          for(var y in x['Time Series (1min)']){
-            this.data1.push(x['Time Series (1min)'][y]["1. open"]);
-            this.data2.push(x['Time Series (1min)'][y]["2. high"]);
-            this.data3.push(x['Time Series (1min)'][y]["3. low"]);
-            this.data4.push(x['Time Series (1min)'][y]["4. close"]);
-            this.data5.push(x['Time Series (1min)'][y]["5. volume"]);
+          for(var y in x['Time Series (5min)']){
+            this.data1.push(x['Time Series (5min)'][y]["1. open"]);
+            this.data2.push(x['Time Series (5min)'][y]["2. high"]);
+            this.data3.push(x['Time Series (5min)'][y]["3. low"]);
+            this.data4.push(x['Time Series (5min)'][y]["4. close"]);
+            this.data5.push(x['Time Series (5min)'][y]["5. volume"]);
             this.labels.push("");
         }
         
