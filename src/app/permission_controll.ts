958faf4ae03cbd38af40c078a159273aecd0b723
rafaelboniolo <33812322+rafaelboniolo@users.noreply.github.com>
2018-12-08 19:34:29 -0200

    busca dos tipos de Pessoa

--- src/app/permission_controll.ts
+++ src/app/permission_controll.ts
@@ -6,21 +6,21 @@ export class Permission{
     
     static execute(router: Router):Boolean{
 
-      if(!localStorage.getItem('currentUser')){
-        router.navigate(['login']);
-        return false;
-      }
+      // if(!localStorage.getItem('currentUser')){
+      //   router.navigate(['login']);
+      //   return false;
+      // }
       
 
-      var x = JSON.parse(localStorage.getItem('currentUser'))['user'];
+      // var x = JSON.parse(localStorage.getItem('currentUser'))['user'];
 
-      if (x["tipo"]  == "ADM" ){
-        router.navigate(['admnistracao']);
-      }else if(x["tipo"] == "INV"){
-        router.navigate(['investimento']);
-      }else if(x["tipo"] == "GES"){
-        router.navigate(['gestao']);
-      }
+      // if (x["tipo"]  == "ADM" ){
+      //   router.navigate(['admnistracao']);
+      // }else if(x["tipo"] == "INV"){
+      //   router.navigate(['investimento']);
+      // }else if(x["tipo"] == "GES"){
+      //   router.navigate(['gestao']);
+      // }
       return true;
     }
 
