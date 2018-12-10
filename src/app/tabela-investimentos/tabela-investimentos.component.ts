39ee3e7f29a847b1a7ae3212201de740422025ca
Gabriel <gabriellsl2009@hotmail.com>
2018-12-09 22:53:22 -0200

    Atualização da tela investimento

--- src/app/tabela-investimentos/tabela-investimentos.component.ts
+++ src/app/tabela-investimentos/tabela-investimentos.component.ts
@@ -11,14 +11,102 @@ import { Subject } from 'rxjs';
 export class TabelaInvestimentosComponent implements OnInit {
 
   depositoEfetuado: Transacao={
-    id_transacao: 0,
+    id_transacao: 1,
     id_investidor: 0,
     id_configtaxa: 0,
     tipo: '+',
-    data: '',
-    valor: 0,
+    data: '22/10/2018',
+    valor: 1500.50,
     status: '',
-    datasaque: '',
+    datasaque: '22/01/2019',
+  }
+
+  depositoEfetuado2: Transacao={
+    id_transacao: 2,
+    id_investidor: 0,
+    id_configtaxa: 0,
+    tipo: '+',
+    data: '30/11/2018',
+    valor: 500.750,
+    status: '',
+    datasaque: '30/03/2019',
+  }
+
+  depositoEfetuado3: Transacao={
+    id_transacao: 3,
+    id_investidor: 0,
+    id_configtaxa: 0,
+    tipo: '+',
+    data: '12/07/2018',
+    valor: 218.35,
+    status: '',
+    datasaque: '12/12/2019',
+  }
+
+  depositoEfetuado4: Transacao={
+    id_transacao: 3,
+    id_investidor: 0,
+    id_configtaxa: 0,
+    tipo: '+',
+    data: '12/07/2018',
+    valor: 218.35,
+    status: '',
+    datasaque: '12/12/2019',
+  }
+
+  depositoEfetuado5: Transacao={
+    id_transacao: 3,
+    id_investidor: 0,
+    id_configtaxa: 0,
+    tipo: '+',
+    data: '12/07/2018',
+    valor: 218.35,
+    status: '',
+    datasaque: '12/12/2019',
+  }
+
+  depositoEfetuado6: Transacao={
+    id_transacao: 3,
+    id_investidor: 0,
+    id_configtaxa: 0,
+    tipo: '+',
+    data: '12/07/2018',
+    valor: 218.35,
+    status: '',
+    datasaque: '12/12/2019',
+  }
+
+  depositoEfetuado7: Transacao={
+    id_transacao: 3,
+    id_investidor: 0,
+    id_configtaxa: 0,
+    tipo: '+',
+    data: '12/07/2018',
+    valor: 218.35,
+    status: '',
+    datasaque: '12/12/2019',
+  }
+
+  depositoEfetuado8: Transacao={
+    id_transacao: 3,
+    id_investidor: 0,
+    id_configtaxa: 0,
+    tipo: '+',
+    data: '12/07/2018',
+    valor: 218.35,
+    status: '',
+    datasaque: '12/12/2019',
+  }
+
+  depositoEfetuado9: Transacao={
+    id_transacao: 3,
+    id_investidor: 0,
+    id_configtaxa: 0,
+    tipo: '+',
+    data: '12/07/2018',
+    valor: 218.35,
+    status: '',
+    datasaque: '12/12/2019',
   }
   
   depositosEfetuados: Transacao[] = new Array;
@@ -28,9 +116,22 @@ export class TabelaInvestimentosComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.buscarDepositosEfetuados();
+    // this.buscarDepositosEfetuados();
+    this.depositosEfetuados.push(this.depositoEfetuado);
+    this.depositosEfetuados.push(this.depositoEfetuado2);
+    this.depositosEfetuados.push(this.depositoEfetuado3);
+    this.depositosEfetuados.push(this.depositoEfetuado4);
+    this.depositosEfetuados.push(this.depositoEfetuado5);
+    // this.depositosEfetuados.push(this.depositoEfetuado6);
+    // this.depositosEfetuados.push(this.depositoEfetuado7);
+    // this.depositosEfetuados.push(this.depositoEfetuado8);
+    // this.depositosEfetuados.push(this.depositoEfetuado9);
     
-    
+  }
+
+  teste(transacao:Transacao){
+    alert(transacao.valor);
+    this.depositosEfetuados.pop;
   }
 
 
